96731f1341d1dde45c8ef4a78576d10f0f7f582b
YanaYushchuk <ia03.yana.yushchuk@gmail.com>
2024-04-30 17:06:28 +0300

    added login logic

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const app = express();
        //Instantiate an express app, the main work horse of this server
 const port = 8080;                  //Save the port number where your server will be listening
 
+app.use(express.static(__dirname));
+
 //Idiomatic expression in express to route and respond to a client request
 app.get('/', (req, res) => {        //get requests to the root ("/") will route here
     res.sendFile('index.html', {root: __dirname});      //server responds by sending the index.html file to the client's browser
